bba601f4bac7dc04c414a78b7bdf1b54a895be19
Royston Martha <rodie98.2@gmail.com>
2025-09-12 22:58:28 -0400

    Cleaned up syntax and used more Swift centric idioms. Also corrected issues with trying to index values in array at indices that don't exist.

--- Musubi/Words.swift
+++ Musubi/Words.swift
@@ -20,10 +20,10 @@ class Kanji {
     var edited = Date.now
     
     fileprivate var _kanji: Character // Literal for itself
-    var on: Array<String> // onyomi
-    var kun: Array<String> // kunyomi
+    var on: [String] // onyomi
+    var kun: [String] // kunyomi
     var meaning = "" // general meaning of kanji
-    var relatedWords: Set<Words> = []
+    var relatedWords: Set<Words> = [] // FIXME: Can't store the actual words... in the JSON.
     
     var literal: Character {
         return _kanji
@@ -50,7 +50,7 @@ class KanjiBank {
         return newKanji // return new kanji
     }
     
-    private init() { }
+    private init() { } // prevent accidentaly extra banks from being created.
     
     func initializeFromFile() {
         // TODO: Load from disk function
@@ -58,6 +58,10 @@ class KanjiBank {
     
     func sync() {
         // TODO: Sync function
+    }
+    
+    func collapseToJSON() -> String {
+        return ""
     }
 }
 
@@ -93,17 +97,18 @@ class Words: Hashable {
     
     init(name: String) {
         self._word = name
+        self._readings = Array.init(name.map { _ in "" })
     }
     
     // Internal Word Representations
     private var _word: String  // Internal String Representation of Word
-    private var _readings: Array<String> = [] // Array of Kanji Readings
+    private var _readings: [String] = [] // Array of Kanji Readings
     private var __readingComplete: Bool = false // An internal value for tracking if a reading is complete
-    private var _mora: Array<String> = [] // An internal array to represent mora
+    private var _mora: [String] = [] // An internal array to represent mora
     
 
     // Alternate writings
-    private var _alternates: Array<String> = []
+    private var _alternates: [String] = []
     
     var meaning = ""
     
@@ -137,47 +142,62 @@ class Words: Hashable {
             return self._word
         }
         set(text) {
-            if text.isJapanese { // If the text is Japanese actually update the word
-                // Update location in WordBank
-                let oldWord = self._word
-                WordBank.bank.words[self._word] = nil
-                self._word = text
-                WordBank.bank.words[text] = self
-                edited = Date.now
-                let oldReading = self._readings
-                self._readings = [] // Clear the readings
-                for i in 0..<self._word.count { // This is set by character count. So - ちゅ is rendered as two characters - and the readings should be such too.
-                    // If a given kanji hasn't changed, keep the reading too.
-                    if Array(oldWord)[i] == Array(self._word)[i] {
-                        self._readings[i] = oldReading[i]
-                        // Don't interact with kanji bank, no new kanji has been introduced or removed.
+            guard text.isJapanese else{ return }
+            // If the text is Japanese actually update the word
+            // Update location in WordBank
+            let oldWord = self._word
+            let oldReading = self._readings
+            
+            WordBank.bank.words.removeValue(forKey: _word) // Remove word from bank
+            self._word = text
+            WordBank.bank.words[text] = self
+            edited = Date.now
+            
+            let chars = Array(_word)
+            let oldChars = Array(oldWord)
+            
+            let minCount = min(chars.count, oldWord.count)
+            
+            self._readings = Array(repeating: "", count: self._word.count) // Clear the readings
+            for i in 0..<minCount { // This is set by character count. So - ちゅ is rendered as one character - and the readings should be such too.
+                let oldChar = oldChars[i]
+                let newChar = chars[i]
+                
+                // If a given kanji hasn't changed, keep the reading too.
+                if oldChar == newChar {
+                    self._readings[i] = oldReading[i]
+                    // Don't interact with kanji bank, no new kanji has been introduced or removed.
+                }else{
+                    if newChar.isKana { // If this charcter is not kanji
+                        self._readings[i] = String(chars[i]) // If the character is hiragana or kana - the reading is itself
                     }else{
-                        if Array(self._word)[i].isKana { // If this charcter is not kanji
-                            self._readings[i] = String(Array(arrayLiteral: self._word)[i]) // If the character is hiragana or kana - the reading is itself
-                        }else{
-                            if self._readings[i].isKanji {
-                                self.kanjis[i] = KanjiBank.bank.newKanji(self._readings[i]) // Add kanji into the self array of kanji
-                                updateKanji()
-                            }
-                            self._readings[i] = "" // Add an empty string.
-                        }
+                        self._readings[i] = "" // Add an empty string. We don't know the reading yet.
                     }
                 }
             }
+            if chars.count > oldChars.count {
+                for i in oldChars.count..<chars.count {
+                    if chars[i].isKana {
+                        _readings[i] = String(chars[i])
+                    } else {
+                        _readings[i] = ""
+                    }
+                }
+            }
+            updateKanji() // This iterates all kanji, this can be excluded from the loop
+            
         }
     }
     
     // Working with the reading as a string
     var reading: String {
         get {
-            var str: String = ""
             for i in 0..<self._readings.count {
                 if self._readings[i] == "" {
                     return "" // Return an empty string if the reading is not complete.
                 }
-                str += self._readings[i]
             }
-            return str
+            return self._readings.joined()
         }
         set(str) {
             if specialReading {  // Special reading means we can't pull from kanji readings. Set manually
@@ -192,7 +212,8 @@ class Words: Hashable {
         }
     }
     
-    var readings: Array<String> {
+    // Working with readings as an array
+    var readings: [String] {
         get {
             return self._readings
         }
@@ -203,36 +224,35 @@ class Words: Hashable {
         }
     }
     
+    // Set a reading. This is always called externally and operates only for kanji characters
     func setReading(_ str: String, at: Int) {
-        if self._word[_word.index(_word.startIndex, offsetBy: at)].isKanji {
-            self._readings[at] = str // There's no check needed because this reading is not a free text field. This is fed in through the kanji character.
-        }
+        let chars = Array(_word) // Grapheme clusters
+        precondition(at >= 0 && at < chars.count, "Index out of bounds in setReading")
+
+        let char = chars[at]
+        precondition(char.isKanji, "setReading called on non-kanji character")
+
+        _readings[at] = str
     }
     
     // Return kanji as an array
-    var kanjis: Array<Kanji?> {
+    var kanjis: [Kanji?] {
         get {
-            var ret: Array<Kanji?> = []
-            for i in 0..<self._word.count {
-                ret[i] = (KanjiBank.bank.kanjis[Array(arrayLiteral: self._word)[i]]) // Return Kanji pulled or nil if no Kanji
+            var ret: [Kanji?] = []
+            for char in Array(self._word) {
+                ret.append(KanjiBank.bank.kanjis[String(char)]) // Return Kanji pulled or nil if no Kanji
             }
             return ret
         }
-            
-        set {
-            // Do nothing. Not how kanji in a word are set.
-        }
     }
     
-    // Return reading as an array
-    
-    
+
     
     // Helper functions
     private func updateKanji() {
-        for i in 0..<self._word.count {
-            if Array(literal)[i].isKanji {
-                if let kanji = KanjiBank.bank.kanjis[Array(arrayLiteral: literal)[i]] {
+        for char in Array(self._word) {
+            if char.isKanji {
+                if let kanji = KanjiBank.bank.kanjis[String(char)] {
                     kanji.relatedWords.insert(self)
                 }
             }
